4f0bd431e8d04232870a02159636159a4dbb7465
Hristoz Ognyanov <hristozognyanov1984@gmail.com>
2024-07-19 14:26:54 +0200

    refactoring and adding some styles

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,14 +1,25 @@
 import './App.css'
-import Hero from './components/hero/Hero'
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Header from './components/header/Header';
+import Hero from './components/hero/Hero';
+import Pricing from './components/pricing/Pricing';
+import Catalog from './components/catalog/Catalog';
+
 
 function App() {
   
   return (
-    <>
-    <Hero />
-    
-    </>
+    <BrowserRouter>
+    <div className="bg-white h-screen">
+      <Header/> 
+      <Routes>
+        <Route path='/' element={<Hero />} />
+        <Route path='/catalog' element={<Catalog />} />
+        <Route path='/pricing' element={<Pricing />} />
+      </Routes>
+    </div>
+    </BrowserRouter>
   )
 }
 
-export default App
+export default App;
--- client/src/components/header/Header.jsx
+++ client/src/components/header/Header.jsx
@@ -1,15 +1,7 @@
 import { useState } from 'react'
 import { Dialog, DialogPanel } from '@headlessui/react'
 import { XMarkIcon, Bars3Icon } from '@heroicons/react/24/outline'
-
-
-
-const navigation = [
-    { name: 'Product', href: '#' },
-    { name: 'Features', href: '#' },
-    { name: 'Marketplace', href: '#' },
-    { name: 'Company', href: '#' },
-  ]
+import { Link } from 'react-router-dom'
 
 function Header() {
     const [mobileMenuOpen, setMobileMenuOpen] = useState(false)
@@ -18,16 +10,18 @@ function Header() {
     <header className="absolute inset-x-0 top-0 z-50">
       
         <nav aria-label="Global" className="flex items-center justify-between p-6 lg:px-8">
+
           <div className="flex lg:flex-1">
-            <a href="#" className="-m-1.5 p-1.5">
-              <span className="sr-only">Your Company</span>
+            <Link to="/" className="-m-1.5 p-1.5">
+              <span className="sr-only">MyNFT</span>
               <img
                 alt="logo"
                 src={'../../../logo1.jpg'}
                 className="h-11 rounded-md w-auto"
               />
-            </a>
+            </Link>
           </div>
+
           <div className="flex lg:hidden">
             <button
               type="button"
@@ -38,32 +32,63 @@ function Header() {
               <Bars3Icon aria-hidden="true" className="h-6 w-6" />
             </button>
           </div>
+
           <div className="hidden lg:flex lg:gap-x-12">
-            {navigation.map((item) => (
-              <a key={item.name} href={item.href} className="text-sm font-semibold leading-6 text-gray-900">
-                {item.name}
-              </a>
-            ))}
+            
+              <Link 
+              to="/"
+              className="text-sm font-semibold leading-6 text-gray-900">
+                Home
+              </Link>
+              <Link 
+              to="/catalog"
+              className="text-sm font-semibold leading-6 text-gray-900">
+                Catalog
+              </Link>
+              <Link 
+              to="/pricing"
+              className="text-sm font-semibold leading-6 text-gray-900">
+                Pricing
+              </Link>
+              <Link
+              to="/about"
+              className="text-sm font-semibold leading-6 text-gray-900">
+                About
+              </Link>
+           
           </div>
-          <div className="hidden lg:flex lg:flex-1 lg:justify-end">
-            <a href="#" className="text-sm font-semibold leading-6 text-gray-900">
-              Log in <span aria-hidden="true">&rarr;</span>
-            </a>
+
+          <div className="hidden lg:flex lg:flex-1 lg:justify-end lg:gap-x-6">
+            <Link 
+            to="#" 
+            className="text-sm font-semibold leading-6 text-gray-900">
+              Log in 
+            <span aria-hidden="true">&rarr;</span>
+            </Link>
+            <Link
+            to="#" 
+            className="text-sm font-semibold leading-6 text-gray-900">
+              Sign up 
+            <span aria-hidden="true">&rarr;</span>
+            </Link>
+
           </div>
+
         </nav>
 
         <Dialog open={mobileMenuOpen} onClose={setMobileMenuOpen} className="lg:hidden">
           <div className="fixed inset-0 z-50" />
           <DialogPanel className="fixed inset-y-0 right-0 z-50 w-full overflow-y-auto bg-white px-6 py-6 sm:max-w-sm sm:ring-1 sm:ring-gray-900/10">
+
             <div className="flex items-center justify-between">
-              <a href="#" className="-m-1.5 p-1.5">
-                <span className="sr-only">Your Company</span>
+              <Link to="/" className="-m-1.5 p-1.5">
+                <span className="sr-only">MyNFT</span>
                 <img
-                  alt=""
-                  src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=600"
+                  alt="logo1"
+                  src={'../../../logo1.jpg'}
                   className="h-8 w-auto"
                 />
-              </a>
+              </Link>
               <button
                 type="button"
                 onClick={() => setMobileMenuOpen(false)}
@@ -73,29 +98,54 @@ function Header() {
                 <XMarkIcon aria-hidden="true" className="h-6 w-6" />
               </button>
             </div>
+
             <div className="mt-6 flow-root">
               <div className="-my-6 divide-y divide-gray-500/10">
+
                 <div className="space-y-2 py-6">
-                  {navigation.map((item) => (
-                    <a
-                      key={item.name}
-                      href={item.href}
-                      className="-mx-3 block rounded-lg px-3 py-2 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-50"
-                    >
-                      {item.name}
-                    </a>
-                  ))}
+                  
+                    <Link
+                      to="/"
+                      className="-mx-3 block rounded-lg px-3 py-2 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-50">
+                      Home
+                    </Link>
+                    <Link
+                      to="/catalog"
+                      className="-mx-3 block rounded-lg px-3 py-2 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-50">
+                      Catalog
+                    </Link>
+                    <Link
+                      to="/pricing"
+                      className="-mx-3 block rounded-lg px-3 py-2 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-50">
+                      Pricing
+                    </Link>
+                    <Link
+                      to="/about"
+                      className="-mx-3 block rounded-lg px-3 py-2 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-50">
+                      About
+                    </Link>
+                  
                 </div>
+
                 <div className="py-6">
-                  <a
-                    href="#"
-                    className="-mx-3 block rounded-lg px-3 py-2.5 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-50"
-                  >
+                  <Link
+                    to="#"
+                    className="-mx-3 block rounded-lg px-3 py-2.5 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-50">
                     Log in
-                  </a>
+                  </Link>
                 </div>
+
+                <div className="py-6">
+                  <Link
+                    to="#"
+                    className="-mx-3 block rounded-lg px-3 py-2.5 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-50">
+                    Sign up
+                  </Link>
+                </div>
+
               </div>
             </div>
+
           </DialogPanel>
         </Dialog>
         
