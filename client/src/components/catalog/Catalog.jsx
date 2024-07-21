2b3aadc8b8e7b95f33fe6a2f6330b639c5568971
Hristoz Ognyanov <hristozognyanov1984@gmail.com>
2024-07-21 17:40:34 +0200

    add spiner

--- client/src/components/catalog/Catalog.jsx
+++ client/src/components/catalog/Catalog.jsx
@@ -1,6 +1,7 @@
-import CardNFT from "./CardNFT";
+
 import nftAPI from "../../api/nftAPI";
 import { useEffect, useState } from "react";
+import CustomSpinner from "../CustomSpinner";
 
 function Catalog() {
 const [nft, setNft] = useState([]);
@@ -14,15 +15,16 @@ const [nft, setNft] = useState([]);
   } , []);
 
     return (
-      <div className="bg-white">
-     
-      <div className="mx-auto max-w-2xl px-4 py-16 sm:px-6 sm:py-24 lg:max-w-7xl lg:px-8">
+      <div className="relative bg-gradient-to-r from-indigo-200 to-yellow-100 px-8 items-center justify-center text-center py-4">
+         
+      <div className="relative mx-auto max-w-2xl px-4 py-16 sm:px-6 sm:py-24 lg:max-w-7xl lg:px-8">
       
       <h2 className="text-2xl font-bold tracking-tight text-gray-900 text-center mb-10">All NFTs</h2>
-      
+          
           <div className="grid grid-cols-1 gap-x-6 gap-y-10 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 xl:gap-x-8">
-
-              {nft.map((nft) => (
+              
+              {nft.length > 0 
+              ? nft.map((nft) => (
               <a key={nft._id} href={nft.imageUrl} className="group">
 
                   <div className="aspect-h-1 aspect-w-1 w-full overflow-hidden rounded-lg bg-gray-200 xl:aspect-h-8 xl:aspect-w-7">
@@ -37,10 +39,14 @@ const [nft, setNft] = useState([]);
                  <p className="mt-1 text-lg font-medium text-gray-900 text-center">{nft.discription}</p>
                  <p className="mt-1 text-lg font-medium text-gray-900 text-center">{nft.prize}</p>
               </a>
-              ))}
-
+              ))
+              : <div className="flex flex-1 items-center justify-center gap-8">
+                <h1 className="text-3xl font-bold">Loding...NFTs</h1>
+                <CustomSpinner />
+               </div>
+              }
+            
           </div>
-      
      </div>
     </div>
     )
