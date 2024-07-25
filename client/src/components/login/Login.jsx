fe1fc1017a370f9e4a4587f28e1fdd47dd7ae7b9
Hristoz Ognyanov <hristozognyanov1984@gmail.com>
2024-07-25 19:37:50 +0200

    add Login Register Component

--- client/src/components/login/Login.jsx
+++ client/src/components/login/Login.jsx
@@ -1,7 +1,49 @@
+import { useContext } from "react";
+import { UserContext } from "../../context/userContext";
+
+import { useForm } from "../../hooks/useFormF";
+
+
+const loginFormKeys = {
+  Email: "email",
+  Password: "password",
+}
+
 function Login() {
+   const { onLoginSubmit } = useContext(UserContext);
+   const { values, changeHandler, onSubmit } = useForm({
+      [loginFormKeys.Email]: '',
+      [loginFormKeys.Password]: '',
+   } , onLoginSubmit );
+
   return (
     <div className ="relative flex flex-grow bg-gradient-to-r from-indigo-200 to-yellow-100 px-8 items-center justify-between py-4">
-      Login
+   
+     <form method="POST" id="login"  onSubmit={onSubmit} className="flex flex-col items-center justify-center">
+
+      <label htmlFor="email">Email:</label>
+      <input 
+      id="email" 
+      type="email" 
+      name={loginFormKeys.Email} 
+      placeholder="email"
+      value={values[loginFormKeys.Email]} 
+      onChange={changeHandler}
+      />
+
+      <label htmlFor="password">Password:</label>
+      <input 
+      id="password" 
+      type="password" 
+      name={loginFormKeys.Password} 
+      placeholder="password" 
+      value={values[loginFormKeys.Password]}
+      onChange={changeHandler}
+      />
+
+      <input type="submit" className="-mx-3 block rounded-lg px-3 py-2 text-base font-semibold leading-7 bg-violet-300 text-gray-900 hover:bg-yellow-100" value="Login" />
+     </form>
+   
     </div>
   )
 }
--- client/src/components/register/Register.jsx
+++ client/src/components/register/Register.jsx
@@ -1,7 +1,14 @@
 function Register() {
     return (
       <div className="relative flex flex-grow bg-gradient-to-r from-indigo-200 to-yellow-100 px-8 items-center justify-between py-4">
-        Register
+       <form>
+      <label>Email</label>
+      <input type="email" placeholder="email"/>
+      <label>Password</label>
+      <input type="password" placeholder="password" />
+      <label>rePassword</label>
+      <input type="password" placeholder="rePassword" />
+     </form>
       </div>
     )
   }
