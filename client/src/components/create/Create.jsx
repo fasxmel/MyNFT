ef507bce2f7fde844e3ff83317a9d54e8b7cb2d8
Hristoz Ognyanov <hristozognyanov1984@gmail.com>
2024-08-06 14:19:23 +0300

    add some style edit create component

--- client/src/components/create/Create.jsx
+++ client/src/components/create/Create.jsx
@@ -5,7 +5,7 @@ import { useCreateNft } from "../../hooks/useNfts";
 const initialValues = {
   title: '',
   description: '',
-  img: '',
+  imageUrl: '',
   price: ''
 }
 
@@ -31,49 +31,113 @@ function Create() {
 
 
   return (
-    <div className="relative flex flex-grow bg-gradient-to-r from-indigo-200 to-yellow-100 px-8 items-center justify-center text-center py-4">
+
+    <div className="flex min-h-full flex-1 flex-col justify-center px-6 py-12 lg:px-8 bg-gradient-to-r from-indigo-200 to-yellow-100 pt-24">
+    <div className="sm:mx-auto sm:w-full sm:max-w-sm">
+      <img
+        alt="Your Company"
+        src="/logo1.jpg"
+        className="mx-auto h-10 w-auto rounded-md"
+      />
+      <h2 className="mt-10 text-center text-2xl font-bold leading-9 tracking-tight text-gray-900">
+        Create NFT
+      </h2>
+    </div>
+
+    <div className="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
+
+      <form onSubmit={submitHandler} className="space-y-6">
+
+        <div>
+          <label htmlFor="title" className="block text-sm font-medium leading-6 text-gray-900">
+           title
+          </label>
+          <div className="mt-2">
+            <input
+              id="title"
+              name="title"
+              type="text"
+              required
+              value={values.title}
+              onChange={changeHandler}
+              className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+            />
+          </div>
+        </div>
+
+        <div>
+          <label htmlFor="description" className="block text-sm font-medium leading-6 text-gray-900">
+          Description
+          </label>
+          
+          <div className="mt-2">
+            <input
+              id="description"
+              name="description"
+              type="text"
+              required
+              value={values.description}
+              onChange={changeHandler}
+              className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+            />
+        </div>
+
+        </div>
+
+        <div>
+          <label htmlFor="imageUrl" className="block text-sm font-medium leading-6 text-gray-900">
+           Image
+          </label>
+          
+          <div className="mt-2">
+            <input
+              id="imageUrl"
+              name="imageUrl"
+              type="text"
+              required
+              value={values.imageUrl}
+              onChange={changeHandler}
+              className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+            />
+        </div>
+
+        </div>
+
+        <div>
+          <label htmlFor="price" className="block text-sm font-medium leading-6 text-gray-900">
+           Price
+          </label>
+          
+          <div className="mt-2">
+            <input
+              id="price"
+              name="price"
+              type="text"
+              required
+              value={values.price}
+              onChange={changeHandler}
+              className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+            />
+        </div>
+
+        </div>
+
+        <div>
+          <button
+            type="submit"
+            className="flex w-full justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+          >
+            Create
+          </button>
+        </div>
+
+      </form>
+
     
-    <form className="flex flex-col items-center justify-center" onSubmit={submitHandler} >
+    </div>
+    </div>
 
-       <label htmlFor="title">Title:</label>
-       <input 
-       type="text" 
-       name="title" 
-       id="title" 
-       value={values.title}
-       onChange={changeHandler}
-       />
 
-       <label htmlFor="description">Description:</label>
-       <textarea 
-       name="description" 
-       id="description"
-       value={values.description}
-       onChange={changeHandler}
-       ></textarea>
-
-       <label htmlFor="imageUrl">Image:</label>
-       <input 
-       type="text" 
-       name="imageUrl" 
-       id="imageUrl" 
-       value={values.image}
-       onChange={changeHandler}
-       />
-
-       <label htmlFor="price">Price:</label>
-       <input 
-       type="text" 
-       name="price" 
-       id="price" 
-       value={values.price}
-       onChange={changeHandler}
-       />
-
-       <button type="submit" value="submit">Create</button>
-    </form>
-
-  </div>
   )
 }
 
