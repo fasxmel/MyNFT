a85c415292db9928d1e823949c9cb8285605a3b9
Hristoz Ognyanov <hristozognyanov1984@gmail.com>
2024-08-06 13:21:04 +0300

    add tailwind component login and register

--- client/src/components/login/Login.jsx
+++ client/src/components/login/Login.jsx
@@ -1,7 +1,8 @@
 import { useForm } from "../../hooks/useFormF";
 import { useLogin } from "../../hooks/useUser";
-import { useNavigate } from "react-router-dom";
+import { useNavigate, Link } from "react-router-dom";
 import { useState } from "react";
+
 
 
 const initialValues = {
@@ -37,36 +38,90 @@ function Login() {
   } = useForm(initialValues, loginHandler);
 
   return (
-    <div className ="relative flex flex-grow bg-gradient-to-r from-indigo-200 to-yellow-100 px-8 items-center justify-between py-4">
-   
-     <form id="login" onSubmit={submitHandler} className="flex flex-col items-center justify-center">
+  
+      <div className="flex min-h-full flex-1 flex-col justify-center px-6 py-12 lg:px-8 bg-gradient-to-r from-indigo-200 to-yellow-100">
 
-      <label htmlFor="email">Email:</label>
-      <input 
-      id="email" 
-      type="email" 
-      name="email"
-      placeholder="email"
-      value={values.email} 
-      onChange={changeHandler}
-      />
+         <div className="sm:mx-auto sm:w-full sm:max-w-sm">
+          <img
+            alt="Your Company"
+            src="/logo1.jpg"
+            className="mx-auto h-10 w-auto rounded-md"
+          />
+          <h2 className="mt-10 text-center text-2xl font-bold leading-9 tracking-tight text-gray-900">
+            Login to your account
+          </h2>
+         </div>
 
-      <label htmlFor="password">Password:</label>
-      <input 
-      id="password" 
-      type="password" 
-      name="password"
-      placeholder="password" 
-      value={values.password}
-      onChange={changeHandler}
-      />
+         <div className="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
 
-      {error && <p className="text-red-500">{error} </p>}
+          <form onSubmit={submitHandler} className="space-y-6">
 
-      <input type="submit" className="-mx-3 block rounded-lg px-3 py-2 text-base font-semibold leading-7 bg-violet-300 text-gray-900 hover:bg-yellow-100" value="Login" />
-     </form>
-   
-    </div>
+            <div>
+
+              <label htmlFor="email" className="block text-sm font-medium leading-6 text-gray-900">
+                Email address
+              </label>
+
+              <div className="mt-2">
+                <input
+                  id="email"
+                  name="email"
+                  type="email"
+                  value={values.email} 
+                  onChange={changeHandler}
+                  required
+                  autoComplete="email"
+                  className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                />
+              </div>
+
+            </div>
+
+            <div>
+            
+                <label htmlFor="password" className="block text-sm font-medium leading-6 text-gray-900">
+                  Password
+                </label>
+                
+              <div className="mt-2">
+                <input
+                  id="password"
+                  name="password"
+                  type="password"
+                  value={values.password} 
+                  onChange={changeHandler}
+                  required
+                  autoComplete="current-password"
+                  className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                />
+              </div>
+
+            </div>
+
+            {error && <p className="text-red-500">{error} </p>}
+
+            <div>
+
+              <button
+                type="submit"
+                className="flex w-full justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+              >
+                Login
+              </button>
+            </div>
+
+          </form>
+
+          <p className="mt-10 text-center text-sm text-gray-500">
+            Not a member?{' '}
+            <Link to="/register" className="font-semibold leading-6 text-indigo-600 hover:text-indigo-500">
+              Start a 14 day free trial
+            </Link>
+          </p>
+         </div>
+
+      </div>
+
   )
 }
 
--- client/src/components/register/Register.jsx
+++ client/src/components/register/Register.jsx
@@ -1,7 +1,7 @@
 import { useState } from "react";
 import { useForm } from "../../hooks/useFormF";
 import { useRegister } from "../../hooks/useUser";
-import { useNavigate } from "react-router-dom";
+import { useNavigate, Link } from "react-router-dom";
 
 const initialValues = {
     email: '',
@@ -35,11 +35,124 @@ function Register() {
    } = useForm(initialValues, registerhandler);
  
   return (
-    <div className ="relative flex flex-grow bg-gradient-to-r from-indigo-200 to-yellow-100 px-8 items-center justify-between py-4">
-   
-    <form id="register" onSubmit={submitHandler} className="flex flex-col items-center justify-center">
 
-     <label htmlFor="email">Email:</label>
+    <>
+
+<div className="flex min-h-full flex-1 flex-col justify-center px-6 py-12 lg:px-8 bg-gradient-to-r from-indigo-200 to-yellow-100">
+
+         <div className="sm:mx-auto sm:w-full sm:max-w-sm">
+          <img
+            alt="Your Company"
+            src="/logo1.jpg"
+            className="mx-auto h-10 w-auto rounded-md"
+          />
+          <h2 className="mt-10 text-center text-2xl font-bold leading-9 tracking-tight text-gray-900">
+            Register your account
+          </h2>
+         </div>
+
+         <div className="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
+
+          <form onSubmit={submitHandler} className="space-y-6">
+
+            <div>
+
+              <label htmlFor="email" className="block text-sm font-medium leading-6 text-gray-900">
+                Email address
+              </label>
+
+              <div className="mt-2">
+                <input
+                  id="email"
+                  name="email"
+                  type="email"
+                  value={values.email} 
+                  onChange={changeHandler}
+                  required
+                  autoComplete="email"
+                  className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                />
+              </div>
+
+            </div>
+
+            <div>
+            
+                <label htmlFor="password" className="block text-sm font-medium leading-6 text-gray-900">
+                  Password
+                </label>
+                
+              <div className="mt-2">
+                <input
+                  id="password"
+                  name="password"
+                  type="password"
+                  value={values.password} 
+                  onChange={changeHandler}
+                  required
+                  autoComplete="current-password"
+                  className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                />
+              </div>
+
+            </div>
+
+            <div>
+            
+                <label htmlFor="password" className="block text-sm font-medium leading-6 text-gray-900">
+                Confirm Password
+                </label>
+                
+              <div className="mt-2">
+                <input
+                  id="rePassword"
+                  name="rePassword"
+                  type="password"
+                  value={values.rePassword} 
+                  onChange={changeHandler}
+                  required
+                  autoComplete="current-password"
+                  className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                />
+              </div>
+
+            </div>
+
+            {error && <p className="text-red-500">{error} </p>}
+
+            <div>
+
+              <button
+                type="submit"
+                className="flex w-full justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+              >
+                Register
+              </button>
+            </div>
+
+          </form>
+
+          <p className="mt-10 text-center text-sm text-gray-500">
+            You already member?{' '}
+            <Link to="/login" className="font-semibold leading-6 text-indigo-600 hover:text-indigo-500">
+              Start a 14 day free trial
+            </Link>
+          </p>
+         </div>
+
+      </div>
+
+
+
+
+
+
+
+    {/* <div className ="relative flex flex-grow bg-gradient-to-r from-indigo-200 to-yellow-100 px-8 items-center justify-between py-4">
+   
+     <form id="register" onSubmit={submitHandler} className="flex flex-col items-center justify-center">
+
+      <label htmlFor="email">Email:</label>
      <input 
      id="email" 
      type="email" 
@@ -76,9 +189,12 @@ function Register() {
 
      <input value="Register" type="submit" className="-mx-3 block rounded-lg px-3 py-2 text-base font-semibold leading-7 bg-violet-300 text-gray-900 hover:bg-yellow-100" />
 
-    </form>
+      </form> 
   
-   </div>
+     </div>  */}
+
+
+    </>
   )
 
 }
